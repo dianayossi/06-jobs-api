36da2e039b1809b6c5768dbfb619540c4887cd09
dianayossi <dycarachure@gmail.com>
2023-01-20 21:44:06 -0800

    week9

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+//connectDB
+const connectDB = require('./db/connect')
+const authenticateUser = require('./middleware/authentication')
+
+//routers
+const authRouther = require('./routes/auth')
+const artistsRouter = require('./routes/artists')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -11,9 +19,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth',authRouther)
+app.use('/api/v1/artists',authenticateUser, artistsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,9 +29,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port,console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
